cf4737e1fc7163f987eba81b6086e41086d205f5
Allison Howard <alliemhow@gmail.com>
2019-08-28 08:53:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function produceDrivingRange(blockRange) {
+  return function(rg1, rg2) {
+    let range = parseInt(rg1) - parseInt(rg2)
+    if (Math.abs(range) > blockRange) {
+      let outOfRange = Math.abs(range) - blockRange
+      return `${outOfRange} blocks out of range`;
+    } else {
+      return `within range by ${Math.abs(range)}`;
+    }
+  }
+}
+
+function produceTipCalculator(percentFare) {
+  return function(fare) {
+    return percentFare * fare
+  }
+}
+
+function createDriver() {
+  let DriverId = 0;
+  return class Driver {
+    constructor(name) {
+      this.name = name
+      this.id = ++DriverId
+    }
+  }
+}
