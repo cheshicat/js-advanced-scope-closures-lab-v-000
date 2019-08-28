5b551884667170e4110b939625d8afd639c49f3d
Allison Howard <alliemhow@gmail.com>
2019-08-28 08:45:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function produceDrivingRange(blockRange) {
   return function(range1, range2) {
     let range = parseInt(range1) - parseInt(range2)
+    if (true) {
+      if (Math.abs(range) > blockRange) {
+        let outOfRange = Math.abs(range) - blockRange
+        return `${outOfRange} blocks out of range`;
+    } else {
+      return `within range by ${Math.abs(range)}`;
+    }
   }
 }
